6a5b8734dbeaa6b934ae09e95294a75fc64d2f86
Marialaceo <beatrizlacerda2011@gmail.com>
2024-05-04 17:50:03 -0300

    Fazendo alterações no script do arquivo processamento_de_dados.sh

--- Sprint_01/Desafio_01/processamento_de_vendas.sh
+++ Sprint_01/Desafio_01/processamento_de_vendas.sh
@@ -2,15 +2,15 @@ mkdir vendas
 cp ecommerce/* vendas
 mkdir vendas/backup
 cp vendas/dados_de_vendas.csv vendas/backup
-mv vendas/backup/dados_de_vendas.csv vendas/backup/backup-dados-20240502.csv
+mv vendas/backup/dados_de_vendas.csv vendas/backup/backup-dados-$(date +"%Y%m%d").csv
 touch vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
 
 date +"%Y/%m/%d %H:%M" > vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
-cat vendas/backup/backup-dados-20240502.csv | cut -d ',' -f 5 | sed -n '2p' >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
-cat vendas/backup/backup-dados-20240502.csv | cut -d ',' -f 5 | sed -n '67p' >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
-cat vendas/backup/backup-dados-20240502.csv | awk -F ',' 'NR > 1 { soma+=$3 } END { print soma }' >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
-head -n 10 vendas/backup/backup-dados-20240502.csv >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
+cat vendas/backup/backup-dados-$(date +"%Y%m%d").csv | cut -d ',' -f 5 | sed -n '2p' >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
+cat vendas/backup/backup-dados-2$(date +"%Y%m%d").csv | cut -d ',' -f 5 | sed -n '67p' >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
+cat vendas/backup/backup-dados-$(date +"%Y%m%d").csv | awk -F ',' 'NR > 1 { soma+=$3 } END { print soma }' >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
+head -n 10 vendas/backup/backup-dados-$(date +"%Y%m%d").csv >> vendas/backup/relatorio$(date +"%Y%m%d-%H%M%S").txt
 
-zip vendas/backup/backup-dados-20240502.zip vendas/backup/backup-dados-20240502.csv
-rm vendas/backup/backup-dados-20240502.csv
+zip vendas/backup/backup-dados-$(date +"%Y%m%d").zip vendas/backup/backup-dados-$(date +"%Y%m%d").csv
+rm vendas/backup/backup-dados-$(date +"%Y%m%d").csv
 
