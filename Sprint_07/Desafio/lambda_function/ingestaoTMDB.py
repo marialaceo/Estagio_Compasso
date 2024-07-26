a43626f2f9ca68dc58e8f638bf57fdbf7c0def1a
Marialaceo <beatrizlacerda2011@gmail.com>
2024-07-26 11:49:12 -0300

    Update - lambda_function/ingestaoTMDB.py; Upload - Evidencias_de_execucao/*
    Atualizando o arquivo de ingestao de dados e enviando os arquivos de print das execuções do desafio

--- Sprint_07/Desafio/lambda_function/ingestaoTMDB.py
+++ Sprint_07/Desafio/lambda_function/ingestaoTMDB.py
@@ -6,26 +6,18 @@ from datetime import datetime
 from requests import get
 from tmdbv3api import TMDb, Genre
 
-# Configurar logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Definir variáveis de ambiente para o S3
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-aws_session_token = os.getenv('AWS_SESSION_TOKEN')
-region_name = os.getenv('AWS_REGION')
-
-# Configurar cliente S3
-s3_client = boto3.client('s3', 
-                      aws_access_key_id=aws_access_key_id,
-                      aws_secret_access_key=aws_secret_access_key,
-                      aws_session_token=aws_session_token,
-                      region_name=region_name)
+s3_client = boto3.client('s3',
+                         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                         aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
+                         region_name=os.getenv('AWS_REGION'))
 
 bucket_name = 'data-lake-final-programa-de-bolsas'
 last_page_key = 'Raw/TMDB/JSON/last_page.txt'
-batch_size = 50  # Número de páginas a processar por execução
+batch_size = 50
 
 def get_last_page():
     try:
@@ -34,7 +26,6 @@ def get_last_page():
         logger.info(f"Última página processada lida: {last_page}")
         return last_page
     except s3_client.exceptions.NoSuchKey:
-        # Se o arquivo não existir, começar do zero
         logger.info("Nenhum registro de página encontrado. Iniciando do início.")
         return 0
 
@@ -52,7 +43,6 @@ def save_to_s3(movies, part_num):
         logger.info("Nenhum filme encontrado.")
         return
 
-    # Prepare the data for processing
     data = [{
         'title': movie.get('title'),
         'release_date': movie.get('release_date'),
@@ -60,29 +50,27 @@ def save_to_s3(movies, part_num):
         'actors': [actor.get('name') for actor in movie.get('credits', {}).get('cast', [])]
     } for movie in movies]
 
-    # Define constants
     max_items_per_file = 100
 
-    # Divide data into chunks of 100 items each
     for i in range(0, len(data), max_items_per_file):
         part_data = data[i:i + max_items_per_file]
         part_json = json.dumps(part_data, ensure_ascii=False)
 
-        # Define the file path for S3
         date_path = datetime.now().strftime('%Y/%m/%d')
         file_path = f'Raw/TMDB/JSON/{date_path}/movies_part_{part_num}_chunk_{i // max_items_per_file + 1}.json'
 
-        # Save the file to S3
-        s3_client.put_object(
-            Bucket=bucket_name,
-            Key=file_path,
-            Body=part_json,
-            ContentType='application/json'
-        )
-        logger.info(f"Arquivo JSON salvo no S3: {file_path}")
+        try:
+            s3_client.put_object(
+                Bucket=bucket_name,
+                Key=file_path,
+                Body=part_json,
+                ContentType='application/json'
+            )
+            logger.info(f"Arquivo JSON salvo no S3: {file_path}")
+        except Exception as e:
+            logger.error(f"Erro ao salvar arquivo no S3: {e}")
 
 def process_romance_movies(tmdb, s3_client, bucket_name, genre_id):
-    # Obter o número total de páginas
     response = get(
         f'https://api.themoviedb.org/3/discover/movie',
         params={
@@ -99,12 +87,11 @@ def process_romance_movies(tmdb, s3_client, bucket_name, genre_id):
     else:
         logger.error(f"Erro ao obter o número total de páginas: {response.status_code} {response.text}")
         return
-    
+
     last_page = get_last_page()
     start_page = last_page + 1
-    max_pages = min(total_pages, 2550)  # Garantir que não exceda o número total de páginas disponível
+    max_pages = min(total_pages, 500)
 
-    # Processar as páginas em lotes
     for current_page in range(start_page, max_pages + 1, batch_size):
         end_page = min(current_page + batch_size - 1, max_pages)
         logger.info(f"Iniciando o processamento de páginas {current_page} a {end_page}.")
@@ -136,16 +123,13 @@ def process_romance_movies(tmdb, s3_client, bucket_name, genre_id):
 
 def lambda_handler(event, context):
     logger.info("Iniciando a função lambda...")
-    
-    # Inicializar a API do TMDb
+
     tmdb = TMDb()
     tmdb.api_key = os.environ.get('TMDB_API_KEY')
-    
-    # Buscar o ID do gênero de romance
+
     logger.info("Buscando o ID do gênero 'Romance'...")
     romance_genre_id = get_genre_id(tmdb, 'Romance')
-    
-    # Verificar se o ID foi encontrado
+
     if not romance_genre_id:
         logger.error("ID do gênero 'Romance' não encontrado.")
         return {
@@ -155,10 +139,9 @@ def lambda_handler(event, context):
 
     logger.info(f"ID do gênero 'Romance': {romance_genre_id}")
 
-    # Buscar e processar filmes de romance
     logger.info("Iniciando o processamento de filmes de romance...")
     process_romance_movies(tmdb, s3_client, bucket_name, romance_genre_id)
-    
+
     logger.info("Ingestão de dados concluída com sucesso.")
     return {
         'statusCode': 200,
@@ -167,17 +150,14 @@ def lambda_handler(event, context):
 
 def get_genre_id(tmdb, genre_name):
     genre = Genre()
-    # Obter lista de gêneros para filmes
     logger.info("Obtendo lista de gêneros para filmes...")
     movie_genres_response = genre.movie_list()
-    
-    # Extrair listas de gêneros
     movie_genres = [g for g in movie_genres_response.get('genres', [])]
-    
+
     for g in movie_genres:
         if g['name'].lower() == genre_name.lower():
             return g['id']
-    
+
     return None
 
 if __name__ == "__main__":
